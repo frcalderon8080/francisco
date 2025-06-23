55b6b9a79a547d7c62d37f15527c969075eee9f2
frcalderon8080 <frankiecalderon87@gmail.com>
2025-06-23 03:40:23 -0500

    Update customer_trusted_to_curated.py

--- project/starter/customer_trusted_to_curated.py
+++ project/starter/customer_trusted_to_curated.py
@@ -1,4 +1,4 @@
-# Join customer_trusted with accelerometer_trusted by email/user
+
 customer_df = glueContext.create_dynamic_frame.from_catalog(
     database="your_database", table_name="customer_trusted"
 )
@@ -12,10 +12,9 @@ joined_df = customer_df.join(
     paths2=["user"]
 )
 
-# Only keep customer fields
 curated_df = joined_df.drop_fields(["user", "x", "y", "z", "timestamp"])
 
-# Write curated customer
+
 glueContext.write_dynamic_frame.from_options(
     frame=curated_df,
     connection_type="s3",
