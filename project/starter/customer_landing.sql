5a81aad87b8160c8346f6b352ae64773b721ca87
frcalderon8080 <frankiecalderon87@gmail.com>
2025-06-23 03:47:18 -0500

    Update and rename customer_landing(.png) to customer_landing.sql

---
+++ project/starter/customer_landing.sql
@@ -0,0 +1,25 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `francisco`.`customer_landing` (
+  `customerName` string,
+  `emails` string,
+  `phone` string,
+  `birthDay` string,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `lastUpdateDate` bigint,
+  `shareWithResearchAsOfDate` bigint,
+  `shareWithPublicAsOfDate` bigint,
+  `shareWithFriendsAsOfDate` bigint
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://cd0030bucket/customers/'
+TBLPROPERTIES ('classification' = 'json');
+
+
+
